6680e065748c2aecd609fac1f141369067952133
User <Willspri@gmail.com>
2025-02-09 08:47:01 +1300

    error fixed

--- toLower.c
+++ toLower.c
@@ -2,20 +2,21 @@
 #include <ctype.h>
 
 void convertToLower(char str[]) {
-	int i = 0;
-	while (str[i] != '\0') {
-		str[i] = tolower(str[i]);
-		i++;
-	}
+    int i = 0;
+    while (str[i] != '\0') {
+        str[i] = tolower(str[i]);
+        i++;
+    }
 }
 
 int main() {
-	char str[100];
+    char str[100];
 
-	printf("Enter string: ");
-	fgets(str, sizeof(str), stdin);
+    printf("Enter string: ");
+    fgets(str, sizeof(str), stdin);
 
+    convertToLower(str);  // Convert to lowercase
+    printf("Converted: %s\n", str);  // Print the converted string
 
-//	convertToLower("Converted: %\n", str);
-	return 0;
+    return 0;
 }
